d98fe3b3eab99c9894bd2b0e2322291b214e5c0d
Gurneet <gurneet.k.arora@gmail.com>
2014-07-07 21:32:37 -0400

    Frontend demo of inventory
    Theme isn't on for the list so we can change the colour of the list item
    depending on how fresh it is.

---
+++ client/frontend/util.js
@@ -0,0 +1,4 @@
+/* TODO
+- function for plural, add s for days or inventory name (since we are just doing carrots and apples
+- make colour scheme for expired, about to expire and good (red, orange, yellow, white?)
+ */
