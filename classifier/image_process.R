788dd7916047493f84bbf1536ef457ca7ec5482f
Clement Fung <clement.y.fung@gmail.com>
2014-07-08 01:50:02 -0400

    Making edits to the IP scripts, using biOps package instead EBImage

--- classifier/image_process.R
+++ classifier/image_process.R
@@ -1,16 +1,25 @@
 
+#########
+#
+# important notes: 
+# pixels can range from 0-255. 255 is full expression.
+# pixels are stored as: [R,G,B]
+# [0,0,0] is a black pixel, [255,255,255] is a white one
+#
+########
+
 extractFeatures <- function(path, print=FALSE) {
 
 	# load libraries for IP
-	library("EBImage");
-	f = readImage(path);
+	library("biOps");
+	f = readJpeg(path);
 
-	object_indices = which(binary_image(f,0.9) == 0);
+	object_indices = which(binary_image(f,220) == 0);
 
 	# really noob way to extract RGB
-	red = mean((imageData(f)[,,1])[object_indices]);
-	green = mean((imageData(f)[,,2])[object_indices]);
-	blue = mean((imageData(f)[,,3])[object_indices]);
+	red = mean((f[,,1])[object_indices]);
+	green = mean((f[,,2])[object_indices]);
+	blue = mean((f[,,3])[object_indices]);
 	roundness = find_roundness(f);
 
 	if (print) {
@@ -29,8 +38,8 @@ extractFeatures <- function(path, print=FALSE) {
 # A function that approximates the "roundness" of an object
 find_roundness <- function(img) {
 
-	bin = binary_image(img, 0.9);
-	hbin = high_pass(bin);
+	bin = binary_image(img, 220);
+	hbin = high_pass(bin, TRUE);
 
 	area = sum(bin == 0);
 	perimeter = sum(hbin == 0);
@@ -41,15 +50,19 @@ find_roundness <- function(img) {
 }
 
 # A function which will perform a high pass filter over an image
-high_pass <- function(img) {
+high_pass <- function(img, binary = TRUE) {
 
 	# load libraries for IP
-	library("EBImage");
+	library("biOps");
 
 	filter_hi = matrix(1, nc=3, nr=3);
 	filter_hi[2,2] = -8;
 
-	img_hi = binary_image(1 - filter2(img, filter_hi));
+	img_hi = imgConvolve(img, filter_hi, 0);
+
+        if (binary) {
+             img_hi = binary_image(255 - img_hi, 220)
+        }
 
 	return(img_hi);
 
@@ -57,16 +70,16 @@ high_pass <- function(img) {
 
 # A function which will perform a binarization of an image
 # t - the threshold over which pixels are sorted
-binary_image <- function(img, t = 0.5) {
+binary_image <- function(img, t = 128) {
 
 	# load libraries for IP
-	library("EBImage");
+	library("biOps");
 
-	img_out = channel(img,"grey");
+	img_out = imagedata(img,"grey");
 	
 	# round the values
-	img_out[img_out >= t] = 1;
-	img_out[img_out < t] = 0;
+	img_out[which(img_out >= t)] = 255;
+	img_out[which(img_out < t)] = 0;
 
 	return(img_out);
 
