86fd34aca54a63c6b1a2509aee40d27546fa82fe
Gurneet <gurneet.k.arora@gmail.com>
2014-07-17 21:04:01 -0400

    footer + better UI

--- client/frontend/util.js
+++ client/frontend/util.js
@@ -2,3 +2,8 @@
 - function for plural, add s for days or inventory name (since we are just doing carrots and apples
 - make colour scheme for expired, about to expire and good (red, orange, yellow, white?)
  */
+$(document).on('pageshow', function () {
+  $.get("footer.html", function(data){
+    $("[data-role='footer']").after(data).trigger("create");
+  });
+}); 
