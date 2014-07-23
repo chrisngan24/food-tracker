9e51fb14c8ef5b23668f01172875021bb819093d
chrisngan24 <chris.ngan24@gmail.com>
2014-07-23 17:04:21 +0000

    Updated training model

--- worker/scripts/test_post.py
+++ worker/scripts/test_post.py
@@ -6,9 +6,10 @@ def test_push_photo(test_url):
     payload = {
             'bucket'    : 'food-tracker',
             'time_in'   : str(time.time()),
-            'camera_id' : '12345',
+            'camera_id' : '012',
             'mode'      : '001',
-            'file_name' : '012-1403293585.jpg'
+            #'file_name' : '012-1406087044.jpg'
+            'file_name' : '012-1406083915.jpg'
 
             }
     r = requests.post(test_url + '/api/v1/photo', data=json.dumps(payload)) 
