e1d6af04957f3241d6f20f036a855733ce5cf107
Chris Ngan <chris.ngan24@gmail.com>
2014-07-28 01:56:27 -0400

    fixed loading issue

--- fridgemate/www/js/main.js
+++ fridgemate/www/js/main.js
@@ -3,8 +3,9 @@ var BASE_URL = "http://shrouded-beyond-1547.herokuapp.com";
 // var BASE_URL = "http://29432714.ngrok.com";
 
 $(document).on('pageinit', '#index', function(){
-  Service.loadData(BASE_URL, userId);
-  $.mobile.changePage("#inventory");
+  Service.loadData(BASE_URL, userId, function(){
+    $.mobile.changePage("#inventory");
+  });
 });
 $(document).on('pageshow','#inventory', function () {
   data = Model.inventory.getInventory(); 
